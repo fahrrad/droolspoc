b0b2d2627de4c3fcf2afb2360609eab26aa41582
Ward Coessens <ward.coessens@gmail.com>
2013-03-07 20:22:02 +0100

    Added dictionary containing vocabulary ( for eclipes spellcheciking

--- src/main/java/com/gb/cropdesign/droolspoc/Plant.java
+++ src/main/java/com/gb/cropdesign/droolspoc/Plant.java
@@ -4,8 +4,27 @@ public class Plant {
 
 	private String name;
 
+	/**
+	 * indicates whether this plant is conform to the rules defined per RPD this
+	 * will probably be defined by a combination of PCR results
+	 */
 	private boolean conform;
 
+	/**
+	 * Does this plant contains any genes introduced during transformation? If
+	 * all the Pcr results are negative, the plant is null segregated, and
+	 * transgene will be null
+	 */
+	private boolean transgene;
+
+	/**
+	 * Zygocity is determined by the copy number of a Gene.
+	 */
+	private Zygocity zygocity;
+
+	/**
+	 * The RPD number for this plant. 
+	 */
 	private String rpd;
 
 	public Plant(String name, String rpd) {
@@ -13,7 +32,6 @@ public class Plant {
 		this.name = name;
 		this.rpd = rpd;
 		this.conform = false;
-
 	}
 
 	public String getName() {
@@ -39,4 +57,22 @@ public class Plant {
 	public void setRpd(String rpd) {
 		this.rpd = rpd;
 	}
+
+	public boolean isTransgene() {
+		return transgene;
+	}
+
+	public void setTransgene(boolean transgene) {
+		this.transgene = transgene;
+	}
+
+	public Zygocity getZygocity() {
+		return zygocity;
+	}
+
+	public void setZygocity(Zygocity zygocity) {
+		this.zygocity = zygocity;
+	}
+	
+	
 }
---
+++ src/main/java/com/gb/cropdesign/droolspoc/Zygocity.java
@@ -0,0 +1,26 @@
+package com.gb.cropdesign.droolspoc;
+
+public enum Zygocity {
+
+	/**
+	 * a cell homozygous is when identical alleles of the gene are present on
+	 * both homologous chromosomes. [wikipedia]
+	 */
+	HOMOZYGOUS("homozygous"),
+
+	/**
+	 * A diploid organism is heterozygous at a gene locus when its cells contain
+	 * two different alleles of a gene. [wikipedia]
+	 */
+	HETEROZYGOUS("heterozygous");
+
+	private final String name;
+
+	private Zygocity(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
